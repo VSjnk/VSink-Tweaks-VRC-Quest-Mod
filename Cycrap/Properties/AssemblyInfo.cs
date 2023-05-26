6f495825b0875ce233124c8b81d4a5ea1c5022e8
VSjnk <100817326+VSjnk@users.noreply.github.com>
2023-05-25 23:58:22 -0700

    Add files via upload

---
+++ Cycrap/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+
+[assembly: System.Reflection.AssemblyTitle(ClientsInfo.Description)]
+[assembly: System.Reflection.AssemblyDescription(ClientsInfo.Description)]
+[assembly: System.Reflection.AssemblyCompany(ClientsInfo.Company)]
+[assembly: System.Reflection.AssemblyProduct(ClientsInfo.Name)]
+[assembly: System.Reflection.AssemblyCopyright(ClientsInfo.Author)]
+[assembly: System.Reflection.AssemblyTrademark(ClientsInfo.Company)]
+[assembly: System.Reflection.AssemblyVersion(ClientsInfo.Version)]
+[assembly: System.Reflection.AssemblyFileVersion(ClientsInfo.Version)]
+[assembly: MelonLoader.MelonInfo(typeof(VSinksModMenu.Main), ClientsInfo.Name, ClientsInfo.Version, ClientsInfo.Author, ClientsInfo.DownloadLink)]
+
+[assembly: MelonColor(System.ConsoleColor.Cyan)]
+[assembly: MelonLoader.MelonGame("VRChat")]
+
+public static class ClientsInfo
+{
+    public const string Name = "VSinkTweaks";
+    public const string Description = "A basic Port of the beat saber mod AnyTweaks";
+    public const string Author = "VSink";
+    public const string Company = "????????";
+    public const string Version = "1.0.0";
+    public const string DownloadLink = "Die";
+}
